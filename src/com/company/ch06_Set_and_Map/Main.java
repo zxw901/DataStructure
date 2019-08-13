5ba9576a7d4a1ac8638ada378122c22840056bae
zxw901 <294594210@qq.com>
2019-08-13 11:30:07 +0800

    第十一章完成

--- src/com/company/ch06_Set_and_Map/Main.java
+++ src/com/company/ch06_Set_and_Map/Main.java
@@ -59,12 +59,12 @@ public class Main {
         System.out.println("Total different words: " + linkedListSet2.getSize());
     }
 
-    public static double testSet(Set<String> set,String filename){
-        long startTime=System.nanoTime();
+    public static double testSet(Set<String> set, String filename) {
+        long startTime = System.nanoTime();
         System.out.println(filename);
         ArrayList<String> words = new ArrayList<>();
 //        FileOperation.readFile("src/com/company/ch06_Set_and_Map/pride-and-prejudice.txt", words1);
-        FileOperation.readFile(filename,words);
+        FileOperation.readFile(filename, words);
         System.out.println("Total words: " + words.size());
 
         for (String word : words) {
@@ -72,137 +72,153 @@ public class Main {
         }
         System.out.println("Total different words: " + set.getSize());
 
-        long endTime= System.nanoTime();
-        return (endTime-startTime)/1000000000.0;
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
     }
 
-    public static void testBSTSetAndLinkedSet(){
-        String filename="src/com/company/ch06_Set_and_Map/pride-and-prejudice.txt";
+    public static void testBSTSetAndLinkedSet() {
+        String filename = "src/com/company/ch06_Set_and_Map/pride-and-prejudice.txt";
 
-        BSTSet<String> bstSet=new BSTSet<>();
-        double time1=testSet(bstSet,filename);
-        System.out.println("BST Set: "+time1+ " s");
+        BSTSet<String> bstSet = new BSTSet<>();
+        double time1 = testSet(bstSet, filename);
+        System.out.println("BST Set: " + time1 + " s");
 
         System.out.println();
 
-        LinkedListSet<String> linkedListSet =new LinkedListSet<>();
-        double time2=testSet(linkedListSet,filename);
-        System.out.println("Linked List Set: "+ time2+" s");
+        LinkedListSet<String> linkedListSet = new LinkedListSet<>();
+        double time2 = testSet(linkedListSet, filename);
+        System.out.println("Linked List Set: " + time2 + " s");
+
+        System.out.println();
+
+        AVLSet<String> avlSet=new AVLSet<>();
+        double time3=testSet(avlSet,filename);
+        System.out.println("AVL Set: "+time3+" s");
     }
 
-    public static void testLinkedMap(){
+    public static void testLinkedMap() {
         System.out.println("Pride and Prejudice");
 
-        ArrayList<String> words=new ArrayList<>();
-        if(FileOperation.readFile("src/com/company/ch06_Set_and_Map/pride-and-prejudice.txt",words)){
-            System.out.println("Total words: "+words.size());
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("src/com/company/ch06_Set_and_Map/pride-and-prejudice.txt", words)) {
+            System.out.println("Total words: " + words.size());
 
-            LinkedListMap<String,Integer> map=new LinkedListMap<>();
+            LinkedListMap<String, Integer> map = new LinkedListMap<>();
             for (String word : words) {
-                if(map.contains(word))
-                    map.set(word,map.get(word)+1);
+                if (map.contains(word))
+                    map.set(word, map.get(word) + 1);
                 else
-                    map.add(word,1);
+                    map.add(word, 1);
             }
 
-            System.out.println("Total different words: "+map.getSize());
-            System.out.println("Frequency of PRIDE: "+map.get("pride"));
-            System.out.println("Frequency of PREJUDICE: "+map.get("prejudice"));
+            System.out.println("Total different words: " + map.getSize());
+            System.out.println("Frequency of PRIDE: " + map.get("pride"));
+            System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
         }
     }
 
-    public static void testBSTMap(){
+    public static void testBSTMap() {
         System.out.println("Pride and Prejudice");
-        long startTime=System.nanoTime();
-        ArrayList<String> words=new ArrayList<>();
-        if(FileOperation.readFile("src/com/company/ch06_Set_and_Map/pride-and-prejudice.txt",words)){
-            System.out.println("Total words: "+words.size());
+        long startTime = System.nanoTime();
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("src/com/company/ch06_Set_and_Map/pride-and-prejudice.txt", words)) {
+            System.out.println("Total words: " + words.size());
             Collections.sort(words);
-            BSTMap<String,Integer> map=new BSTMap<>();
+            BSTMap<String, Integer> map = new BSTMap<>();
             for (String word : words) {
-                if(map.contains(word))
-                    map.set(word,map.get(word)+1);
+                if (map.contains(word))
+                    map.set(word, map.get(word) + 1);
                 else
-                    map.add(word,1);
+                    map.add(word, 1);
             }
-            long endTime=System.nanoTime();
-            double time=(endTime-startTime)/1000000000.0;
-            System.out.println("Total different words: "+map.getSize());
-            System.out.println("Frequency of PRIDE: "+map.get("pride"));
-            System.out.println("Frequency of PREJUDICE: "+map.get("prejudice"));
-            System.out.println("BST: "+time+" s");
+            long endTime = System.nanoTime();
+            double time = (endTime - startTime) / 1000000000.0;
+            System.out.println("Total different words: " + map.getSize());
+            System.out.println("Frequency of PRIDE: " + map.get("pride"));
+            System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
+            System.out.println("BST: " + time + " s");
         }
     }
 
-    public static double testAVL(AVLTree<String,Integer> avl,String filename){
-        long startTime=System.nanoTime();
+    public static double testAVL(AVLTree<String, Integer> avl, String filename) {
+        long startTime = System.nanoTime();
         System.out.println(filename);
         ArrayList<String> words = new ArrayList<>();
 //        FileOperation.readFile("src/com/company/ch06_Set_and_Map/pride-and-prejudice.txt", words1);
-        FileOperation.readFile(filename,words);
+        FileOperation.readFile(filename, words);
         System.out.println("Total words: " + words.size());
-        Collections.sort(words);
+//        Collections.sort(words);
 
         for (String word : words) {
-            if(avl.contains(word))
-                avl.set(word,avl.get(word)+1);
+            if (avl.contains(word))
+                avl.set(word, avl.get(word) + 1);
             else
-                avl.add(word,1);
+                avl.add(word, 1);
         }
-        System.out.println("Total different words: " + avl.getSize());
-        System.out.println("isBST : "+avl.isBST());
-        System.out.println("isBalanced : "+avl.isBalanced());
-        long endTime= System.nanoTime();
-        double time=(endTime-startTime)/1000000000.0;
-        System.out.println("AVL: "+time+" s");
-        return (endTime-startTime)/1000000000.0;
+//        System.out.println("Total different words: " + avl.getSize());
+//        System.out.println("isBST : "+avl.isBST());
+//        System.out.println("isBalanced : "+avl.isBalanced());
+        for (String word : words) {
+            avl.remove(word);
+            if (!avl.isBST() || !avl.isBalanced())
+                throw new RuntimeException("Error");
+        }
+        long endTime = System.nanoTime();
+        double time = (endTime - startTime) / 1000000000.0;
+        System.out.println("AVL: " + time + " s");
+        return (endTime - startTime) / 1000000000.0;
     }
 
-    public static double testMap(Map<String,Integer> map,String filename){
-        long startTime=System.nanoTime();
+    public static double testMap(Map<String, Integer> map, String filename) {
+        long startTime = System.nanoTime();
         System.out.println(filename);
         ArrayList<String> words = new ArrayList<>();
 //        FileOperation.readFile("src/com/company/ch06_Set_and_Map/pride-and-prejudice.txt", words1);
-        FileOperation.readFile(filename,words);
+        FileOperation.readFile(filename, words);
         System.out.println("Total words: " + words.size());
 
         for (String word : words) {
-            if(map.contains(word))
-                map.set(word,map.get(word)+1);
+            if (map.contains(word))
+                map.set(word, map.get(word) + 1);
             else
-                map.add(word,1);
+                map.add(word, 1);
         }
         System.out.println("Total different words: " + map.getSize());
 
-        long endTime= System.nanoTime();
-        return (endTime-startTime)/1000000000.0;
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
     }
 
-    public static void testBSTMapAndLinkedMap(){
-        String filename="src/com/company/ch06_Set_and_Map/pride-and-prejudice.txt";
+    public static void testBSTMapAndLinkedMap() {
+        String filename = "src/com/company/ch06_Set_and_Map/pride-and-prejudice.txt";
 
-        BSTMap<String,Integer> bstSet=new BSTMap<>();
-        double time1=testMap(bstSet,filename);
-        System.out.println("BST Map: "+time1+ " s");
+        BSTMap<String, Integer> bstMap = new BSTMap<>();
+        double time1 = testMap(bstMap, filename);
+        System.out.println("BST Map: " + time1 + " s");
 
         System.out.println();
 
-        LinkedListMap<String,Integer> linkedListSet =new LinkedListMap<>();
-        double time2=testMap(linkedListSet,filename);
-        System.out.println("Linked List Map: "+ time2+" s");
+        LinkedListMap<String, Integer> linkedListMap = new LinkedListMap<>();
+        double time2 = testMap(linkedListMap, filename);
+        System.out.println("Linked List Map: " + time2 + " s");
+
+        System.out.println();
+
+        AVLMap<String,Integer> avlMap=new AVLMap<>();
+        double time3=testMap(avlMap,filename);
+        System.out.println("AVL Map: "+time3+" s");
     }
 
     public static void main(String[] args) {
 //        testBSTSet();
 //        testLinkedListSet();
-//        testBSTSetAndLinkedSet();
+        testBSTSetAndLinkedSet();
 //        testLinkedMap();
 //        testBSTMap();
 //        testBSTMapAndLinkedMap();
-        AVLTree<String,Integer> avl=new AVLTree<String,Integer>();
-        testAVL(avl,"pride-and-prejudice.txt");
-        testBSTMap();
-
+//        AVLTree<String, Integer> avl = new AVLTree<String, Integer>();
+//        testAVL(avl, "pride-and-prejudice.txt");
+//        testBSTMap();
     }
 
 }
--- src/com/company/ch11_AVLTree/AVLTree.java
+++ src/com/company/ch11_AVLTree/AVLTree.java
@@ -2,271 +2,281 @@ package com.company.ch11_AVLTree;
 
 import java.util.ArrayList;
 
-public class AVLTree <K extends Comparable<K>,V> {
+public class AVLTree<K extends Comparable<K>, V> {
 
-        public class Node {
-            private K key;
-            private V value;
-            private Node left;
-            private Node right;
-            private int height;
+    public class Node {
+        private K key;
+        private V value;
+        private Node left;
+        private Node right;
+        private int height;
 
-            public Node(K key, V value) {
-                this.key = key;
-                this.value = value;
-                this.left = null;
-                this.right = null;
-                this.height=1;
-            }
-
-            public Node(K key) {
-                this(key, null);
-            }
-
-            public Node() {
-                this(null, null);
-            }
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.left = null;
+            this.right = null;
+            this.height = 1;
         }
 
-        private Node root;
-        private int size;
-
-        public AVLTree() {
-            root = null;
-            size = 0;
+        public Node(K key) {
+            this(key, null);
         }
 
-        //获得节点 node 的高度
-        private int getHeight(Node node){
-            if(node==null)
-                return 0;
-            return node.height;
+        public Node() {
+            this(null, null);
         }
+    }
 
-        //获得节点 node 的平衡因子
-        private int getBalanceFactor(Node node){
-            if(node==null)
-                return 0;
-            return getHeight(node.left)-getHeight(node.right);
-        }
+    private Node root;
+    private int size;
 
-        //判断该二叉树是否是一棵二分搜索树
-        public boolean isBST(){
-            ArrayList<K> keys=new ArrayList<>();
-            inOrder(root,keys);
-            for (int i = 1; i < keys.size(); i++) {
-                if(keys.get(i-1).compareTo(keys.get(i))>0)
-                    return false;
-            }
-            return true;
-        }
+    public AVLTree() {
+        root = null;
+        size = 0;
+    }
 
-        private void inOrder(Node node,ArrayList<K> keys){
-            if(node==null)
-                return;
-            inOrder(node.left,keys);
-            keys.add(node.key);
-            inOrder(node.right,keys);
-        }
+    //获得节点 node 的高度
+    private int getHeight(Node node) {
+        if (node == null)
+            return 0;
+        return node.height;
+    }
 
-        //判断该二叉树是否是一棵平衡二叉树
-        public boolean isBalanced(){
-            return isBalanced(root);
-        }
+    //获得节点 node 的平衡因子
+    private int getBalanceFactor(Node node) {
+        if (node == null)
+            return 0;
+        return getHeight(node.left) - getHeight(node.right);
+    }
 
-        private boolean isBalanced(Node node){
-            if(node==null)
-                return true;
-            if(getBalanceFactor(node)>1)
+    //判断该二叉树是否是一棵二分搜索树
+    public boolean isBST() {
+        ArrayList<K> keys = new ArrayList<>();
+        inOrder(root, keys);
+        for (int i = 1; i < keys.size(); i++) {
+            if (keys.get(i - 1).compareTo(keys.get(i)) > 0)
                 return false;
-            return isBalanced(node.left)&&isBalanced(node.right);
+        }
+        return true;
+    }
+
+    private void inOrder(Node node, ArrayList<K> keys) {
+        if (node == null)
+            return;
+        inOrder(node.left, keys);
+        keys.add(node.key);
+        inOrder(node.right, keys);
+    }
+
+    //判断该二叉树是否是一棵平衡二叉树
+    public boolean isBalanced() {
+        return isBalanced(root);
+    }
+
+    private boolean isBalanced(Node node) {
+        if (node == null)
+            return true;
+        if (getBalanceFactor(node) > 1)
+            return false;
+        return isBalanced(node.left) && isBalanced(node.right);
+    }
+
+    //寻找二分搜索树的最小元素
+    public V minimum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("AVLTree is empty!");
+        }
+        return minimum(root).value;
+    }
+
+    private Node minimum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    private Node getNode(Node node, K key) {
+        if (node == null)
+            return null;
+        if (key.compareTo(node.key) < 0)
+            return getNode(node.left, key);
+        else if (key.compareTo(node.key) > 0)
+            return getNode(node.right, key);
+        else
+            return node;
+    }
+
+    private Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T3 = x.right;
+
+        x.right = y;
+        y.left = T3;
+
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    private Node leftRotate(Node y) {
+        Node x = y.right;
+        Node T2 = x.left;
+
+        x.left = y;
+        y.right = T2;
+
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    public void add(K key, V value) {
+        root = add(root, key, value);
+    }
+    private Node add(Node node, K key, V value) {
+        if (node == null) {
+            size++;
+            return new Node(key, value);
+        }
+        if (key.compareTo(node.key) < 0)
+            node.left = add(node.left, key, value);
+        else if (key.compareTo(node.key) > 0)
+            node.right = add(node.right, key, value);
+        else
+            node.value = value;
+
+        //更新 height
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+
+        //计算平衡因子
+        int balanceFactor = getBalanceFactor(node);
+//            if(Math.abs(balanceFactor)>1)
+//                System.out.println("unbalanced : "+balanceFactor);
+
+        //维护平衡
+        //LL
+        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0)
+            return rightRotate(node);
+
+        //RR
+        if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0)
+            return leftRotate(node);
+
+        //LR
+        if (balanceFactor > 1 && getBalanceFactor(node.left) < 0) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
         }
 
-        //寻找二分搜索树的最小元素
-        public V minimum() {
-            if (size == 0) {
-                throw new IllegalArgumentException("AVLTree is empty!");
-            }
-            return minimum(root).value;
+        //RL
+        if (balanceFactor < -1 && getBalanceFactor(node.right) > 0) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
         }
 
-        private Node minimum(Node node) {
-            if (node.left == null) {
-                return node;
-            }
-            return minimum(node.left);
+        return node;
+    }
+
+    public V remove(K key) {
+        Node node = getNode(root, key);
+        if (node != null) {
+            root = remove(root, key);
+            return node.value;
+        }
+        return null;
+    }
+
+    private Node remove(Node node, K key) {
+
+        if (node == null) {
+            return null;
         }
 
-        //删除树中的最小值
-        public V removeMin() {
-            V ret = minimum();
-            root = removeMin(root);
-            return ret;
-        }
-
-        private Node removeMin(Node node) {
+        Node retNode;
+        if (key.compareTo(node.key) < 0) {
+            node.left = remove(node.left, key);
+            retNode = node;
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = remove(node.right, key);
+            retNode = node;
+        } else {
             if (node.left == null) {
                 Node rightNode = node.right;
                 node.right = null;
                 size--;
-                return rightNode;
-            }
-            node.left = removeMin(node.left);
-            return node;
-        }
-
-        private Node getNode(Node node, K key) {
-            if (node == null)
-                return null;
-            if (key.compareTo(node.key) < 0)
-                return getNode(node.left, key);
-            else if (key.compareTo(node.key) > 0)
-                return getNode(node.right, key);
-            else
-                return node;
-        }
-
-        private Node rightRotate(Node y){
-            Node x=y.left;
-            Node T3=x.right;
-
-            x.right=y;
-            y.left=T3;
-
-            y.height=Math.max(getHeight(y.left),getHeight(y.right))+1;
-            x.height=Math.max(getHeight(x.left),getHeight(x.right))+1;
-
-            return x;
-        }
-
-        private Node leftRotate(Node y){
-            Node x=y.right;
-            Node T2=x.left;
-
-            x.left=y;
-            y.right=T2;
-
-            y.height=Math.max(getHeight(y.left),getHeight(y.right))+1;
-            x.height=Math.max(getHeight(x.left),getHeight(x.right))+1;
-
-            return x;
-        }
-
-        public void add(K key, V value) {
-            root = add(root, key, value);
-        }
-
-        private Node add(Node node, K key, V value) {
-            if (node == null) {
-                size++;
-                return new Node(key, value);
-            }
-            if (key.compareTo(node.key) < 0)
-                node.left = add(node.left, key, value);
-            else if (key.compareTo(node.key) > 0)
-                node.right = add(node.right, key, value);
-            else
-                node.value = value;
-
-            //更新 height
-            node.height=1+Math.max(getHeight(node.left),getHeight(node.right));
-
-            //计算平衡因子
-            int balanceFactor=getBalanceFactor(node);
-//            if(Math.abs(balanceFactor)>1)
-//                System.out.println("unbalanced : "+balanceFactor);
-
-            //维护平衡
-            //LL
-            if(getBalanceFactor(node)>1&&getBalanceFactor(node.left)>=0)
-                return rightRotate(node);
-
-            //RR
-            if(getBalanceFactor(node)<-1&&getBalanceFactor(node.right)<=0)
-                return leftRotate(node);
-
-            //LR
-            if(getBalanceFactor(node)>1&&getBalanceFactor(node.left)<0)
-            {
-                node.left=leftRotate(node.left);
-                return rightRotate(node);
-            }
-
-            //RL
-            if(getBalanceFactor(node)<-1&&getBalanceFactor(node.right)>0)
-            {
-                node.right=rightRotate(node.right);
-                leftRotate(node);
-            }
-
-            return node;
-        }
-
-        public V remove(K key) {
-            Node node = getNode(root, key);
-            if (node != null) {
-                root = remove(root, key);
-                return node.value;
-            }
-            return null;
-        }
-
-        private Node remove(Node node, K key) {
-
-            if (node == null) {
-                return null;
-            }
-
-            if (key.compareTo(node.key) < 0) {
-                node.left = remove(node.left, key);
-                return node;
-            }
-            else if (key.compareTo(node.key) > 0) {
-                node.right = remove(node.right, key);
-                return node;
-            }
-            else {
-                if (node.left == null) {
-                    Node rightNode = node.right;
-                    node.right = null;
-                    size--;
-                    return rightNode;
-                }
-                if (node.right == null) {
-                    Node leftNode = node.left;
-                    node.left = null;
-                    size--;
-                    return leftNode;
-                }
+                retNode = rightNode;
+            } else if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                retNode = leftNode;
+            } else {
                 Node successor = minimum(node.right);
-                successor.right = removeMin(node.right);
+                successor.right = remove(node.right, successor.key);
                 successor.left = node.left;
                 node.left = node.right = null;
-                return successor;
+                retNode = successor;
             }
         }
 
-        public boolean contains(K key) {
-            return getNode(root, key) != null;
+        if(retNode==null)
+            return null;
+
+        //更新 height
+        retNode.height = Math.max(getHeight(retNode.left), getHeight(retNode.right)) + 1;
+
+        //计算平衡因子
+        int balanceFactor = getBalanceFactor(retNode);
+
+        //维护平衡
+        //LL
+        if (balanceFactor > 1 && getBalanceFactor(retNode.left) >= 0)
+            return rightRotate(retNode);
+
+        //RR
+        if (balanceFactor < -1 && getBalanceFactor(retNode.right) <= 0)
+            return leftRotate(retNode);
+
+        //LR
+        if (balanceFactor > 1 && getBalanceFactor(retNode.left) < 0) {
+            retNode.left = leftRotate(retNode.left);
+            return rightRotate(retNode);
         }
 
-        public void set(K key, V value) {
-            Node node = getNode(root, key);
-            if (node == null)
-                throw new IllegalArgumentException(key + " doesn't exist!");
-            node.value = value;
+        //RL
+        if (balanceFactor < -1 && getBalanceFactor(retNode.right) > 0) {
+            retNode.right = rightRotate(retNode.right);
+            return leftRotate(retNode);
         }
 
-        public V get(K key) {
-            Node node = getNode(root, key);
-            return node == null ? null : node.value;
-        }
+        return retNode;
+    }
 
-        public boolean isEmpty() {
-            return size == 0;
-        }
+    public boolean contains(K key) {
+        return getNode(root, key) != null;
+    }
 
-        public int getSize() {
-            return size;
-        }
+    public void set(K key, V value) {
+        Node node = getNode(root, key);
+        if (node == null)
+            throw new IllegalArgumentException(key + " doesn't exist!");
+        node.value = value;
+    }
+
+    public V get(K key) {
+        Node node = getNode(root, key);
+        return node == null ? null : node.value;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
