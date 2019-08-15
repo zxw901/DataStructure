5f35c6b342ae39e078f1aa59264e682d43ab4742
zxw901 <294594210@qq.com>
2019-08-15 16:49:54 +0800

    第十三章完成

--- src/com/company/ch13_HashTable/HashTable.java
+++ src/com/company/ch13_HashTable/HashTable.java
@@ -3,24 +3,26 @@ package com.company.ch13_HashTable;
 import java.util.TreeMap;
 
 public class HashTable<K, V> {
+
+    private final int[] capacity
+            ={53,97,193,389,769,1543,3079,6151,12289,24593,
+            49157,98317,196613,393241,786433,1572869,3145739,6291469,
+            12582917,25165843,50331653,100663319,201326611,402653189,805306457,1610612741};
+
     private static final int upperTol=10;
     private static final int lowerTol=2;
-    private static final int initCapacity=7;
+    private  int capacityIndex=0;
     private int M;
     private int size;
     private TreeMap<K, V>[] hashTable;
 
-    public HashTable(int M) {
-        this.M = M;
+    public HashTable() {
+        this.M = capacity[capacityIndex];
         size = 0;
         hashTable = new TreeMap[M];
         for (int i = 0; i < M; i++) {
             hashTable[i] = new TreeMap<>();
         }
-    }
-
-    public HashTable() {
-        this(initCapacity);
     }
 
     private int hash(K key) {
@@ -39,8 +41,9 @@ public class HashTable<K, V> {
             map.put(key, value);
             size++;
         }
-        if(size>=upperTol*M){
-            resize(2*M);
+        if(size>=upperTol*M&&capacityIndex+1<capacity.length){
+            capacityIndex++;
+            resize(capacity[capacityIndex]);
         }
 
     }
@@ -52,8 +55,8 @@ public class HashTable<K, V> {
             res = map.remove(key);
             size--;
         }
-        if(size<lowerTol*M&&M/2>=initCapacity){
-            resize(M/2);
+        if(size<lowerTol*M&&capacityIndex-1>=0){
+            resize(capacity[capacityIndex]);
         }
         return res;
     }
